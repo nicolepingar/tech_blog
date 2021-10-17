1591d7e8b34eafd5de49f606fc560997723c181f
Nicole Pingar <nicolepingar@yahoo.com>
2021-10-17 14:37:37 -0400

    started main formatting handlebars

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const { User } = require('../../models');
-
 // CREATE a new user
 router.post('/', async (req, res) => {
     try {
@@ -8,10 +7,8 @@ router.post('/', async (req, res) => {
             username: req.body.username,
             password: req.body.password,
         });
-
         req.session.save(() => {
             req.session.loggedIn = true;
-
             res.status(200).json(dbUserData);
         });
     } catch (err) {
@@ -19,7 +16,6 @@ router.post('/', async (req, res) => {
         res.status(500).json(err);
     }
 });
-
 // Login
 router.post('/login', async (req, res) => {
     try {
@@ -28,7 +24,6 @@ router.post('/login', async (req, res) => {
                 username: req.body.username,
             },
         });
-
         if (!dbUserData) {
             res
                 .status(400)
@@ -37,14 +32,12 @@ router.post('/login', async (req, res) => {
         }
 
         const validPassword = await dbUserData.checkPassword(req.body.password);
-
         if (!validPassword) {
             res
                 .status(400)
                 .json({ message: 'Incorrect username or password. Please try again!' });
             return;
         }
-
         req.session.save(() => {
             req.session.loggedIn = true;
 
@@ -57,7 +50,6 @@ router.post('/login', async (req, res) => {
         res.status(500).json(err);
     }
 });
-
 // Logout
 router.post('/logout', (req, res) => {
     if (req.session.loggedIn) {
@@ -68,5 +60,4 @@ router.post('/logout', (req, res) => {
         res.status(404).end();
     }
 });
-
 module.exports = router;
